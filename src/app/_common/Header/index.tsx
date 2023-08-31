8e3909c43fbc13c28bb7ca2db6e9f7bb664c3865
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-08-31 13:56:32 +0900

    header footer 작업시작

---
+++ src/app/_common/Header/index.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+
+    return (
+        <header>
+            <nav>
+                <div>로고</div>
+                <div>
+                    
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from './_common/Header';
+import Footer from './_common/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
